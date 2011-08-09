a81cb8573af166d2f6ecc882789d4e9dfb2eca8d
Érico Andrei <erico@simplesconsultoria.com.br>
2011-08-08 22:41:37 -0300

    Add a string interpolation

---
+++ apyb/pythonbrasil/adapters.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+from zope.component import adapts
+
+from Products.CMFCore.interfaces import IContentish
+
+from apyb.pythonbrasil import MessageFactory as _
+from plone.stringinterp.interfaces import IStringSubstitution
+from plone.stringinterp.adapters import BaseSubstitution
+
+class EmailSubstitution(BaseSubstitution):
+    adapts(IContentish)
+    
+    category = _(u'All Content')
+    description = _(u'Content E-mail')
+    
+    def safe_call(self):
+        return self.context.email
